6d4059569a2b7ab9950301e583296202d767ac08
Sávio Oliveira <saviooliveiratec@gmail.com>
2024-06-01 18:48:54 -0300

    Update App.js

--- modulo10/template-materialui/src/App.js
+++ modulo10/template-materialui/src/App.js
@@ -1,11 +1,11 @@
-import Header from "./components/header";
-import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+import React from 'react';
+import './App.css'; // Supondo que você está usando CSS para estilização
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <ListarTarefa />
+    <div className="App" style={{ backgroundColor: 'lightblue' }}>
+      <h1>Hello, world!</h1>
     </div>
   );
 }
